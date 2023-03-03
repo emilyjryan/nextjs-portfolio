c4e4daa062fdcfd1f39fac80272b6c923323e846
emilyjryan <emilyryanmac@gmail.com>
2023-03-03 13:21:31 -0800

    styled about

--- pages/contact.js
+++ pages/contact.js
@@ -5,10 +5,10 @@ export default function Contact() {
         <div>
             <h1>Contact Me!</h1>
 
-            <h2><a href="https://www.linkedin.com/in/emilyjudithryan/">LinkedIn</a></h2>
-            <h2><a href="https://github.com/emilyjryan">GitHub</a></h2>
-            <h2>Email me @: emilyryanmac@gmail.com</h2>
-            <h2>Cell: 805.315.5707</h2>
+            <h4><a href="https://www.linkedin.com/in/emilyjudithryan/">LinkedIn</a></h4>
+            <h4><a href="https://github.com/emilyjryan">GitHub</a></h4>
+            <h4>Email me @: emilyryanmac@gmail.com</h4>
+            <h4>Cell: 805.315.5707</h4>
 
         </div>
     )
--- pages/projects.js
+++ pages/projects.js
@@ -10,10 +10,10 @@ export default function Projects() {
             <h2>Language Quiz</h2>
             <h3>This 10-question language quiz includes audio samples of each phrase and optional hints to aid the user in identification.</h3>
             <h4>Tech stack used: Javascript, HTML, CSS</h4>
-            <br/>
             <a href="https://github.com/emilyjryan/project-one-language-quiz">Github Repo</a>
             <br/>
             <a href="https://emilyjryan.github.io/project-one-language-quiz/">Live Deployment</a>
+            <br/>
             <br/>
             <Image src={"/pics/language-game/welcome.png"} alt="welcome" width={400} height={300}/>
            
@@ -27,10 +27,11 @@ export default function Projects() {
             <h2>DiscoverDrug</h2>
             <h3>DiscoverDrug allows users to learn about specific drugs using the FDA's vast API, save different drugs as favorites, and add personal comments.</h3>
             <h4>Tech stack used: Javascript, Node.js, HTML, Bootstrap, SQL</h4>
-            <br/>
+           
             <a href="https://github.com/emilyjryan/project-two">Github Repo</a>
             <br/>
             <a href="https://discover-drug-app-deploy.herokuapp.com/">Live Deployment</a>
+            <br/>
             <br/>
             <Image src={"/pics/discover-drug/welcome.png"} alt="welcome" width={400} height={200}/>
           
@@ -46,10 +47,11 @@ export default function Projects() {
             <h2>Sustain-ABLE</h2>
             <h3>This MERN-stack app gives users ideas for daily habits to promote a sustainable lifestyle. Users can create a profile, save favorite habits, and create new habits.</h3>
             <h4>Tech stack used: React, Mongo, Mongoose, Bootstrap</h4>
-            <br/>
+  
             <a href="https://github.com/emilyjryan/sustainABLE-client">Github Repo</a>
             <br/>
             <a href="https://main--vermillion-melba-389997.netlify.app/">Live Deployment</a>
+            <br/>
             <br/>
             <Image src={"/pics/sustain-able/welcome.png"} alt="welcome" width={400} height={300}/>
       
@@ -65,10 +67,11 @@ export default function Projects() {
             <h2>Regulate</h2>
             <h3>Regulate is an app for kids with autism to promote independence by helping them create a structured schedule and identify complex emotions.</h3>
             <h4>Tech stack used: Typescript, React, Mongo, Bulma</h4>
-            <br/>
+    
             <a href="https://github.com/emilyjryan/regulate-app">Github Repo</a>
             <br/>
             <a href="https://unique-lollipop-eb6497.netlify.app/">Live Deployment</a>
+            <br/>
             <br/>
             <Image src={"/pics/regulate/homepage.png"} alt="homepage" width={400} height={350}/>
        
