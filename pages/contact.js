9b1e87962e7d0e227d63a84ab6701e62bdea6314
emilyjryan <emilyryanmac@gmail.com>
2023-03-03 11:55:33 -0800

    added resume screenshot

--- pages/contact.js
+++ pages/contact.js
@@ -5,6 +5,11 @@ export default function Contact() {
         <div>
             <h1>Contact Me!</h1>
 
+            <h2><a href="https://www.linkedin.com/in/emilyjudithryan/">LinkedIn</a></h2>
+            <h2><a href="https://github.com/emilyjryan">GitHub</a></h2>
+            <h2>Email me @: emilyryanmac@gmail.com</h2>
+            <h2>Cell: 805.315.5707</h2>
+
         </div>
     )
 }
--- pages/resume.js
+++ pages/resume.js
@@ -1,9 +1,11 @@
 import styles from "../styles/Resume.module.css"
+import Image from "next/image"
 
 export default function Resume() {
     return (
         <div>
             <h1>My Resume</h1>
+            <Image src="/pics/Emily_Ryan_Resume_Mar 2023.png" alt="Emily Ryan resume" width={400} height={500}/>
 
         </div>
     )
