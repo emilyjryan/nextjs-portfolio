0255a04716961115cd22324125167535a9a5b15d
emilyjryan <emilyryanmac@gmail.com>
2023-04-11 00:28:33 -0700

    fixed skill divs

--- components/Main.js
+++ components/Main.js
@@ -16,7 +16,7 @@ const Main = () => {
                     A Full-Stack Software Engineer
                 </h1>
                 <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
-                    I'm a software engineer with a background in biology and healthcare, ready to make a difference in the world of biotech
+                    I'm a software engineer with a background in biology and healthcare, ready to make a difference in the world of biotech.
                 </p>
                 <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
                     <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
--- components/Skills.js
+++ components/Skills.js
@@ -6,60 +6,57 @@ const Skills = () => {
     <div className='w-full lg:h-screen p-2'>
         <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
             <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Skills</p>
-            <h2 className='py-4'>What I'm Capable Of</h2>
+            <h2 className='py-4'>My Tech Stack</h2>
+
             <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
 
-            </div>
-            <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                <div className='m-auto'>
-                    <Image
-                        src='/../public/assets/skills/html.png'
-                        alt='/'
-                        width='64'
-                        height='64'
-                    />
-                </div>
-                <div className='flex flex-col items-center justify-center'>
-                    <h3>HTML</h3>
-                </div>
-
-            </div>
-
-            </div>
-            <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                <div className='m-auto'>
-                    <Image
-                        src='/../public/assets/skills/css.png'
-                        alt='/'
-                        width='64'
-                        height='64'
-                    />
-                </div>
-                <div className='flex flex-col items-center justify-center'>
-                    <h3>CSS</h3>
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                        <div className='m-auto'>
+                            <Image
+                                src='/../public/assets/skills/html.png'
+                                alt='/'
+                                width='64'
+                                height='64'
+                            />
+                        </div>
+                        <div className='flex flex-col items-center justify-center'>
+                            <h3>HTML</h3>
+                        </div>
+                    </div>
                 </div>
 
-            </div>
-
-            </div>
-            <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                <div className='m-auto'>
-                    <Image
-                        src='/../public/assets/skills/js.png'
-                        alt='/'
-                        width='64'
-                        height='64'
-                    />
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                        <div className='m-auto'>
+                            <Image
+                                src='/../public/assets/skills/css.png'
+                                alt='/'
+                                width='64'
+                                height='64'
+                            />
+                        </div>
+                        <div className='flex flex-col items-center justify-center'>
+                            <h3>CSS</h3>
+                        </div>
+                    </div>
                 </div>
-                <div className='flex flex-col items-center justify-center'>
-                    <h3>Javascript</h3>
+
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                        <div className='m-auto'>
+                            <Image
+                                src='/../public/assets/skills/js.png'
+                                alt='/'
+                                width='64'
+                                height='64'
+                            />
+                        </div>
+                        <div className='flex flex-col items-center justify-center'>
+                            <h3>JavaScript</h3>
+                        </div>
+                    </div>
                 </div>
-
-            </div>
-
             </div>
         </div>
     </div>
