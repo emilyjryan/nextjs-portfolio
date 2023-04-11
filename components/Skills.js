54a7282c145ec7719549267c1cd17ed74adbc8b0
emilyjryan <emilyryanmac@gmail.com>
2023-04-11 00:10:55 -0700

    added some skills

--- components/Skills.js
+++ components/Skills.js
@@ -27,6 +27,40 @@ const Skills = () => {
             </div>
 
             </div>
+            <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                <div className='m-auto'>
+                    <Image
+                        src='/../public/assets/skills/css.png'
+                        alt='/'
+                        width='64'
+                        height='64'
+                    />
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                    <h3>CSS</h3>
+                </div>
+
+            </div>
+
+            </div>
+            <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                <div className='m-auto'>
+                    <Image
+                        src='/../public/assets/skills/js.png'
+                        alt='/'
+                        width='64'
+                        height='64'
+                    />
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                    <h3>Javascript</h3>
+                </div>
+
+            </div>
+
+            </div>
         </div>
     </div>
   )
