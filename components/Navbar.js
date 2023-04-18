5444d91a0424552dfbc2254a68c6157b829cc4a3
emilyjryan <emilyryanmac@gmail.com>
2023-04-18 14:00:13 -0700

    fixed logo on hamburger menu

--- components/Navbar.js
+++ components/Navbar.js
@@ -89,7 +89,7 @@ const Navbar = () => {
                     : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'}>
                 <div>
                     <div className='flex w-full items-center justify-between'>
-                        <Image src='/../public/assets/initials-logo.png' width='87' height='35' alt='/' />
+                        <Image src={logo} width='125' height='50' alt='/' />
                         <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
                             <AiOutlineClose/>
                         </div>
